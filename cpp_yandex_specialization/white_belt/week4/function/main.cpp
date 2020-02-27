a3910c988c9c4c931e24171ce8ad0598e0519b28
AlexKaravaev <karavaev-sasha@mail.ru>
2020-02-27 11:54:30 +0300

    Function added

---
+++ cpp_yandex_specialization/white_belt/week4/function/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+class FunctionPart {
+public:
+    FunctionPart(char new_operation, double new_value) {
+        operation = new_operation;
+        value = new_value;
+    }
+    double Apply(const double& source_value) const {
+        if (operation == '+') {
+            return source_value + value;
+        }
+        if (operation == '-') {
+            return source_value - value;
+        }
+        if (operation == '*') {
+            return source_value * value;
+        }
+        if (operation == '/') {
+            return source_value / value;
+        }
+        return 0;
+    }
+    void Invert() {
+        if (operation == '+') {
+            operation = '-';
+        } 
+        else if (operation == '-') {
+            operation = '+';
+        }
+        else if (operation == '*') { 
+            operation = '/';
+        }
+        else {
+            operation = '*';
+        }
+    }
+private:
+    char operation;
+    double value;
+};
+
+class Function {
+public:
+    
+    void AddPart(char operation, double value){
+        parts.push_back({operation, value});
+    }
+    
+    double Apply(double value) const {
+        for (const FunctionPart& part : parts) {
+            value = part.Apply(value);
+        }
+        return value;
+    }
+    
+    void Invert() {
+        for (FunctionPart& part : parts) {
+            part.Invert();
+        }
+        reverse(begin(parts), end(parts));
+    }
+private:
+    vector<FunctionPart> parts;
+};
+
