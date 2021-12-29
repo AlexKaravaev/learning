739f9475e6d65292f7a72b9b11ed45db96f4ea5c
AlexKaravaev <alexkaravev@gmail.com>
2021-12-29 21:17:11 +0300

    Add minigrep program

---
+++ rust/rust_programming_book/12/minigrep/src/lib.rs
@@ -0,0 +1,62 @@
+use std::error::Error;
+use std::fs;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+pub struct Config {
+
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config{
+    pub fn new(args: &[String]) -> Result<Config, &str>
+    {
+        if args.len() != 3 {
+            return Err("There should be only 2 cli arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+
+        Ok(Config{query, filename})
+
+    }
+
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+    Ok(())
+}
