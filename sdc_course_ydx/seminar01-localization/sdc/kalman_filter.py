d3a3777bc16f1bfc49979b2c6320140a8c32bc3e
AlexKaravaev <alexkaravev@gmail.com>
2022-03-18 20:02:54 +0300

    Add func for kalman filter steps

--- sdc_course_ydx/seminar01-localization/sdc/kalman_filter.py
+++ sdc_course_ydx/seminar01-localization/sdc/kalman_filter.py
@@ -8,12 +8,8 @@ def kalman_transit_covariance(S, A, R):
     :param A: Either transition matrix or jacobian matrix
     :param R: Current noise covariance matrix
     """
-    state_size = S.shape[0]
-    assert S.shape == (state_size, state_size)
-    assert A.shape == (state_size, state_size)
-    assert R.shape == (state_size, state_size)
-    new_S = np.dot(np.dot(A, S), A.T) + R
-    return new_S
+    new_s = np.dot(np.dot(S, A), S.T) + R
+    return new_s
 
 
 def kalman_process_observation(mu, S, observation, C, Q):
@@ -25,15 +21,8 @@ def kalman_process_observation(mu, S, observation, C, Q):
     :param C: Observation matrix
     :param Q: Noise covariance matrix (with zero mean)
     """
-    state_size = mu.shape[0]
-    observation_size = observation.shape[0]
-    assert S.shape == (state_size, state_size)
-    assert observation_size == C.shape[0]
-    assert observation_size == Q.shape[0]
-    H = np.linalg.inv(np.dot(np.dot(C, S), C.T) + Q)
-    K = np.dot(np.dot(S, C.T), H)
-    new_mu = mu + np.dot(K, observation - np.dot(C, mu))
-    new_S = np.dot(np.eye(state_size) - np.dot(K, C), S)
-    # Избавляемся от маленьких чисел. Из-за них могут быть мнимые числа в собственных значениях
-    new_S[np.abs(new_S) < 1e-16] = 0
-    return new_mu, new_S
+    new_k = np.dot(np.dot(S,C.T),np.linalg.inv(np.dot(np.dot(C,S), C.T) + Q))
+    new_mu = mu + np.dot(new_k, (observation-np.dot(C, mu)))
+    new_s = np.dot((np.eye(mu.shape[0]) - np.dot(new_k, C)),S)
+
+    return new_mu, new_s
